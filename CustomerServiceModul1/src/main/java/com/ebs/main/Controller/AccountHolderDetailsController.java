684ed83f851abb4696718f8da086d55053947f9a
poojapatil <poojapatil1622@gmail.com>
2024-12-09 15:12:48 +0530

    change

--- CustomerServiceModul1/src/main/java/com/ebs/main/Controller/AccountHolderDetailsController.java
+++ CustomerServiceModul1/src/main/java/com/ebs/main/Controller/AccountHolderDetailsController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,7 +30,7 @@ public class AccountHolderDetailsController
 					return new ResponseEntity<AccountHolderDetails>(accDetailsRef,HttpStatus.CREATED);
 	}
 	
-	@GetExchange("/overall-accountCustomer")
+	@GetMapping("/overall-accountCustomer")
 	public ResponseEntity<Iterable<AccountHolderDetails>> ongetAccountDetails()
 	{
 			Iterable<AccountHolderDetails> accRef=accHolDetailsServiceI.getCustomerAccount();
