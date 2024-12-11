9389940c5170aca6f6d96d310e599d400049d7a8
poojapatil <poojapatil1622@gmail.com>
2024-12-11 14:19:04 +0530

    ATM methods

--- CustomerServiceModul1/src/main/java/com/ebs/main/serviceImpl/ATMDetailsServiceImpl.java
+++ CustomerServiceModul1/src/main/java/com/ebs/main/serviceImpl/ATMDetailsServiceImpl.java
@@ -1,10 +1,13 @@
 package com.ebs.main.serviceImpl;
 
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ebs.main.model.ATMDetails;
 import com.ebs.main.repository.ATMDetailsRepository;
 import com.ebs.main.serviceI.ATMDetailsServiceI;
 
@@ -14,4 +17,39 @@ public class ATMDetailsServiceImpl implements ATMDetailsServiceI
 	private static final Logger LOG=LoggerFactory.getLogger(ATMDetailsServiceImpl.class);
 	
 	@Autowired ATMDetailsRepository atmDetailsRepository;
-}
+
+	@Override
+	public ATMDetails saveATMDetails(ATMDetails atmDetails) {
+		if(atmDetails !=null)
+		{
+			return atmDetailsRepository.save(atmDetails);
+		}
+		
+		else {
+			return null; // Alternatively, throw an exception if saving a null object is not allowed
+		}
+	}
+
+	@Override
+	public ATMDetails getATMDetailsById(long id) {
+			if (atmDetailsRepository.existsById(null)) 
+			{
+				Optional<ATMDetails> idRef = atmDetailsRepository.findById(id);
+				if (idRef.isPresent())
+				{
+					return idRef.get();
+				}
+				else {
+					return null; 
+				}
+			}
+				else {
+					return null;
+				}
+			}
+		
+		
+	}
+	
+	
+
